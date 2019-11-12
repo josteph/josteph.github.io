42f7440aa12f36c27327247885e41aba508282c4
Joshua Stephen <jostephen22@yahoo.com>
2019-11-12 20:36:30 +0700

    Updates

--- precache-manifest.4a99be89d2ddfc584abec8918612c5d3.js
+++ precache-manifest.4a99be89d2ddfc584abec8918612c5d3.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "50c8270983335999960773f0c497a6c9",
+    "revision": "87af3962d1d85a24e5d72f456c372ec0",
     "url": "/index.html"
   },
   {
-    "revision": "8860960718dcbefd054f",
-    "url": "/static/css/main.162e918c.chunk.css"
+    "revision": "617c339b0af4825e80a9",
+    "url": "/static/css/main.d9e55292.chunk.css"
   },
   {
     "revision": "9af957dd5b034011c9eb",
     "url": "/static/js/3.96102606.chunk.js"
   },
   {
-    "revision": "76c69d92cf3ad17bcf05",
-    "url": "/static/js/home.6c22467a.chunk.js"
+    "revision": "293ec5f6d21bff6be432",
+    "url": "/static/js/home.e19172da.chunk.js"
   },
   {
-    "revision": "8860960718dcbefd054f",
+    "revision": "617c339b0af4825e80a9",
     "url": "/static/js/main.e8a3045c.chunk.js"
   },
   {
-    "revision": "e3c26958bfbcfc97bed2",
-    "url": "/static/js/runtime-main.0ba86a01.js"
+    "revision": "c1f96fd1bccbc0559200",
+    "url": "/static/js/runtime-main.633709a1.js"
   },
   {
     "revision": "51b4a619fab5e1a38643657191a85aa5",
