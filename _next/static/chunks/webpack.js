7894dcbe470b487dbe972e1410bbd6898b132c4e
Joshua Stephen <jostephen22@yahoo.com>
2021-03-08 15:50:28 +0700

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d8520af3e7002a7359e2";
+/******/ 	var hotCurrentHash = "7554404daf42eaa250d2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
